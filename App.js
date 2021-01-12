f2bd65daa9e1f3dc45ae77803e9a9641e20b2883
Shaivi Bansal <76728969+shaivi98@users.noreply.github.com>
2021-01-12 14:53:23 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+  function App() {
+    return (
+    <div className="container">
     <div className="App">
-       <form>
-            <h1><u>REGISTRATION</u></h1>
-            <fieldset>
-              
-                  <label for="firstname">Firstname:</label><br />
-                  <input type="text" id="firstname" required/><br />
-               
-                  <label for="lastname">Lastname:</label><br />
-                  <input type="lastname" id="lastname" required/><br />
-               
-                  <label for="contact">Contact Number:</label><br />
-                  <input type="contact" id="contact" required/><br />
-               
-            </fieldset>
-            <button>Submit</button>
-          </form>
-     
-    </div>
-  );
-}
+      <form>
+        <h1><u>Login</u></h1>
+        <label>
+          Username:
+          <input type="text"  />
+        </label>
+        <label>
+          Password:
+          <input type="password"  />
+        </label>
+        <button><input type="submit" value="Submit" /></button>
+      </form>
+      </div>
+      </div>
+    );
+  }
+
 
 export default App;
